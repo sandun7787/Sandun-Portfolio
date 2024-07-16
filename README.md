b8ffd55aa9ae97695f0682cc399cc0e45be33dbb
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-07-16 22:35:25 +0530

    ongoing-develop-portfolio

---
+++ README.md
@@ -0,0 +1,108 @@
+# My-Portfoliyo - Personal Web Site
+<br>
+<h5 align="center">
+𝚃𝚑𝚊𝚗𝚔 𝚈𝚘𝚞 𝙵𝚘𝚛 𝚈𝚘𝚞𝚛 𝙲𝚘𝚖𝚒𝚗𝚐 this Repository 😍😍😍<br>
+𝙷𝚊𝚟𝚎 𝚊 nice 𝚍𝚊𝚢 ! 
+	<img src="https://raw.githubusercontent.com/isharamaduranga/red-alpha/main/Hi.gif" width="40px" Height="40px">
+</h5>
+<div align="center">
+
+![repo size](https://img.shields.io/github/repo-size/isharamaduranga/My-Portfolio?label=Repo%20Size&style=for-the-badge&labelColor=black&color=1eb61e)
+![GitHub forks](https://img.shields.io/github/forks/isharamaduranga/My-Portfolio?&labelColor=black&color=2196f3&style=for-the-badge)
+![GitHub stars](https://img.shields.io/github/stars/isharamaduranga/My-Portfolio?&labelColor=black&color=ff9800&style=for-the-badge)
+![GitHub LastCommit](https://img.shields.io/github/last-commit/isharamaduranga/My-Portfolio?logo=github&labelColor=black&color=e91e63&style=for-the-badge)
+</div>
+
+## Description
+#### This website designs for showing some information about my career.I categorize them into few sections...
+* Home page
+* Who am I
+* What I Top Skills
+* What are the technologies & Tools I use
+* What kind of services I provide
+* Projects & assignments I have done
+* My Educational Background
+* My contact details
+
+#### Also,This website Optimized to Provide for All Mobile & Desktop Devices Can View 
+
+## :link: Reference Links
+
+🌱 Moke-Up-URL ---> [Click Me!](https://www.figma.com/proto/4mJXjz2mPQsSg21Im2JLeV/My_PortFilo_Mock-up?page-id=0%3A1&node-id=14%3A3&scaling=min-zoom&starting-point-node-id=14%3A3)<br>
+🌱 Site-Map-URL----> [Click Me!](https://www.gloomaps.com/2KQ3vDp779)<br>
+🌱 Wire-Frame-URL----> [Click Me!](https://wireframe.cc/pQAVpH)<br>
+🌱 GitHub-Pages-URL----> [Click Me!](https://isharamaduranga.github.io/My-Portfolio/)<br>
+🌱 InfinitiFree-WebHost-URL----> [Click Me!](http://isharamaduranga.epizy.com)
+
+## Basic Background of PortFolio
+<img src="assets/Readme/My_PortFilo_Mock-up-1.png">
+
+## Technology /framework used
+* HTML
+* CSS
+* Java Script
+
+## License
+**Copyright @ 2022 My PortFoliyo All Right Reserved By Ishara Maduranga**
+
+<br>
+<br>
+
+# Web Pos System(Bootstrap) - Super-Market
+#### Also,This website Optimized to Provide for All Mobile & Desktop Devices Can View
+
+## :link: Reference Links
+
+🌱 Moke-Up-URL ---> [Click Me!](https://www.figma.com/file/kJ5dbaCDIMbBH6S3eyZJwM/POS_System(Bootstrap)?node-id=0%3A1)<br>
+🌱 Site-Map-URL----> [Click Me!](https://www.gloomaps.com/3XQDYVDqdC)<br>
+🌱 Wire-Frame-URL----> [Click Me!](https://wireframe.cc/9oCnTI)<br>
+
+## ✔ Description
+#### This website designs for showing some proceed of web pos system.I categorize them into few Functionlities ...
+### Manage Customer
+* Register Customer
+* Unregister Customer
+* Find Customer
+* Update Customer
+* Get All Customers
+
+### Manage Store
+* Add Items
+* Remove Items
+* Search Items
+* Update Items
+* Get All Items
+
+### Place Order
+* Save a Customer Order
+* Search Customer Order;
+* Update Customer Order
+* Delete Customer Order;
+* Get All Customers Order
+
+## Basic Background of Pos_System
+
+### 🌱 DashBoard Interface
+<img src="assets/web_pos_pics_for-ReadMe/dashboard.png">
+
+### 🌱 Place Order Manage Interface
+<img src="assets/web_pos_pics_for-ReadMe/ord.png">
+
+### 🌱 Customers Manage Interface
+<img src="assets/web_pos_pics_for-ReadMe/cus.png">
+
+### 🌱 Items Manage Interface
+<img src="assets/web_pos_pics_for-ReadMe/itm.png">
+
+### 🌱 Order-Details Interface
+<img src="assets/web_pos_pics_for-ReadMe/od_detail.png">
+
+
+## Technology /framework used
+* HTML
+* CSS
+* Java Script
+* Bootstrap-V5.2
+
+## License
+**Copyright @ 2022 Web_Pos_System All Right Reserved By Ishara Maduranga**
---
+++ js/script.js
@@ -0,0 +1,136 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+
+
+    /* toggle menu/ navbar script */
+
+    $('.menu-btn').click(function () {
+
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+
+    });
+
+    //side-up script
+
+    $('#scroll-up-btn').click(function() {
+        $('html').animate({scrollTop:0});
+    });
+
+
+    /*typing animation script*/
+    var typed = new Typed(".typing",{
+        strings:["Full Stack Developer","WEB-DEVELOPER","BACK-END DEVELOPER","FRONT-END DEVELOPER","SOFTWARE DESIGNER","UI/UX DESIGNER"],
+        typeSpeed:80,
+        backSpeed:50,
+        loop:true
+    });
+
+
+    var typed = new Typed(".typing_about",{
+        strings:["Full Stack Developer","WEB-DEVELOPER","BACK-END DEVELOPER","FRONT-END DEVELOPER","SOFTWARE DESIGNER","UI/UX DESIGNER"],
+        typeSpeed:80,
+        backSpeed:50,
+        loop:true
+    });
+
+    var typed = new Typed(".typing_about2",{
+        strings:["Full Stack Developer","WEB-DEVELOPER","BACK-END DEVELOPER","FRONT-END DEVELOPER","SOFTWARE DESIGNER","UI/UX DESIGNER"],
+        typeSpeed:80,
+        backSpeed:50,
+        loop:true
+    });
+});
+
+/*// jQuery counterUp
+$('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 250
+});*/
+
+/* Displayed  default Home scripts..  */
+$("#home_nav").css({'padding-bottom':'15px','border-radius':'10px','border-bottom':'2px solid lime'});
+$("#about_nav").css('border-bottom','none');
+$("#skill_nav").css('border-bottom','none');
+$("#service_nav").css('border-bottom','none');
+$("#project_nav").css('border-bottom','none');
+$("#education_nav").css('border-bottom','none');
+$("#contact_nav").css('border-bottom','none');
+
+$("#home_nav").click(function () {
+    $("#home_nav").css({'padding-bottom':'15px','border-radius':'10px','border-bottom':'2px solid lime'});
+    $("#about_nav").css('border-bottom','none');
+    $("#skill_nav").css('border-bottom','none');
+    $("#service_nav").css('border-bottom','none');
+    $("#project_nav").css('border-bottom','none');
+    $("#education_nav").css('border-bottom','none');
+    $("#contact_nav").css('border-bottom','none');
+});
+
+$("#about_nav").click(function () {
+    $("#about_nav").css({'padding-bottom':'15px','border-radius':'10px','border-bottom':'2px solid lime'});
+    $("#home_nav").css('border-bottom','none');
+    $("#skill_nav").css('border-bottom','none');
+    $("#service_nav").css('border-bottom','none');
+    $("#project_nav").css('border-bottom','none');
+    $("#education_nav").css('border-bottom','none');
+    $("#contact_nav").css('border-bottom','none');
+});
+
+$("#skill_nav").click(function () {
+    $("#skill_nav").css({'padding-bottom':'15px','border-radius':'10px','border-bottom':'2px solid lime'});
+    $("#home_nav").css('border-bottom','none');
+    $("#about_nav").css('border-bottom','none');
+    $("#service_nav").css('border-bottom','none');
+    $("#project_nav").css('border-bottom','none');
+    $("#education_nav").css('border-bottom','none');
+    $("#contact_nav").css('border-bottom','none');
+});
+
+$("#service_nav").click(function () {
+    $("#service_nav").css({'padding-bottom':'15px','border-radius':'10px','border-bottom':'2px solid lime'});
+    $("#home_nav").css('border-bottom','none');
+    $("#about_nav").css('border-bottom','none');
+    $("#skill_nav").css('border-bottom','none');
+    $("#project_nav").css('border-bottom','none');
+    $("#education_nav").css('border-bottom','none');
+    $("#contact_nav").css('border-bottom','none');
+});
+
+$("#project_nav").click(function () {
+    $("#project_nav").css({'padding-bottom':'15px','border-radius':'10px','border-bottom':'2px solid lime'});
+    $("#home_nav").css('border-bottom','none');
+    $("#skill_nav").css('border-bottom','none');
+    $("#service_nav").css('border-bottom','none');
+    $("#about_nav").css('border-bottom','none');
+    $("#education_nav").css('border-bottom','none');
+    $("#contact_nav").css('border-bottom','none');
+});
+
+$("#education_nav").click(function () {
+    $("#education_nav").css({'padding-bottom':'15px','border-radius':'10px','border-bottom':'2px solid lime'});
+    $("#home_nav").css('border-bottom','none');
+    $("#skill_nav").css('border-bottom','none');
+    $("#service_nav").css('border-bottom','none');
+    $("#project_nav").css('border-bottom','none');
+    $("#about_nav").css('border-bottom','none');
+    $("#contact_nav").css('border-bottom','none');
+});
+
+$("#contact_nav").click(function () {
+    $("#contact_nav").css({'padding-bottom':'15px','border-radius':'10px','border-bottom':'2px solid lime'});
+    $("#home_nav").css('border-bottom','none');
+    $("#skill_nav").css('border-bottom','none');
+    $("#service_nav").css('border-bottom','none');
+    $("#project_nav").css('border-bottom','none');
+    $("#education_nav").css('border-bottom','none');
+    $("#about_nav").css('border-bottom','none');
+});
+
